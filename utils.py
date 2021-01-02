2b780abfa1e2f1a730b307a783202186289bce02
wangxin <wangxin@91cyt.com>
2021-01-02 17:23:23 +0800

    1

--- utils.py
+++ utils.py
@@ -29,11 +29,47 @@ class RedisClient():
         else:
             return self._setup_redis()
 
-    def set_cache(self, name, key, value, cache_cycle=7):
+    def set_cache(self, name, key, value, cache_cycle=7, refresh=False):
         self.db.hset(name, key, value)
-        if self.db.ttl(name) <= 0:
-            self.db.expire(name, cache_cycle * 24 * 3600)
+        if self.db.ttl(name) <= 0 or refresh:
+            self.db.expire(name, cache_cycle)
+
+    def get_cache(self, name, key):
+        """ 
+
+        Args:
+            name ([type]): [description]
+            key ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        cache = self.db.hget(name, key)
+        return cache
+
+    def cache(self, name, key, value, cache_cycle=7, refresh: bool = False):
+        """
+        获取和设置缓存
+
+        :param name: 哈希表名
+        :type name: string
+        :param key: 键名
+        :type key: str
+        :param value: 键的值
+        :type value: optional
+        :param cache_cycle: 缓存时间, defaults to 7s
+        :type cache_cycle: int, optional
+        :param refresh: 是否刷新缓存时间, defaults to False
+        :type refresh: bool, optional
+        :return: 值
+        :rtype: any
+        """
+        cache_data = self.get_cache(name, key)
+        if not cache_data:
+            self.set_cache(name, key, value, cache_cycle, refresh)
+        return value
 
 
 if __name__ == "__main__":
-    pass
+    r = RedisClient()
+    r.set_cache('test1', 'key', 1, cache_cycle=20, refresh=True)
