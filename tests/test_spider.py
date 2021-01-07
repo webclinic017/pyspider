779782e4ac6f3480e6d41654dd0fa664269e6d23
wangxin <wangxin@91cyt.com>
2021-01-07 14:48:31 +0800

    1

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -1,7 +1,9 @@
 import asyncio
 import sys
-sys.path.append('..')
+import os
+sys.path.append(os.pardir)
 from common.spider import AsyncSpider
+from config.db_setup import RedisClient
 
 
 async def fetch_page():
@@ -16,6 +18,15 @@ def test_async_spider():
     res = asyncio.run(fetch_page())
     # print(res)
     assert res is not None
+
+
+def test_redis_client():
+    redis_client = RedisClient()
+    new_var = 'pytest'
+    cache_cycle = 60
+    redis_client.set_cache(new_var, 'test', 'test_pytest', cache_cycle=60)
+    assert redis_client.exists(new_var) == 1
+    assert redis_client.ttl(new_var) == cache_cycle
 
 
 if __name__ == "__main__":
