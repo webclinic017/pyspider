a5d6c8f7c3f40b74e6efb6113b7c2abbe01159b1
wangxin <eric@example.com>
2021-01-10 22:07:42 +0800

    1

---
+++ utils/ulities.py
@@ -0,0 +1,44 @@
+import re
+
+_ITERABLE_SINGLE_VALUES = dict, str, bytes
+
+
+def arglist_to_dict(arglist):
+    """Convert a list of arguments like ['arg1=val1', 'arg2=val2', ...] to a
+    dict
+    """
+    return dict(x.split('=', 1) for x in arglist)
+
+
+def regex(x):
+    if isinstance(x, str):
+        return re.compile(x)
+    return x
+
+
+def unique(list_, key=lambda x: x):
+    """efficient function to uniquify a list preserving item order"""
+    seen = set()
+    result = []
+    for item in list_:
+        seenkey = key(item)
+        if seenkey in seen:
+            continue
+        seen.add(seenkey)
+        result.append(item)
+    return result
+
+
+def arg_to_iter(arg):
+    """Convert an argument to an iterable. The argument can be a None, single
+    value, or an iterable.
+
+    Exception: if arg is a dict, [arg] will be returned
+    """
+    if arg is None:
+        return []
+    elif not isinstance(arg, _ITERABLE_SINGLE_VALUES) and hasattr(
+            arg, '__iter__'):
+        return arg
+    else:
+        return [arg]
