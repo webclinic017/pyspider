4ff3aeb641612fa305669cc6feb8f343a37bd5e5
wangxin <eric@example.com>
2021-02-13 16:44:19 +0800

    1

--- draft.py
+++ draft.py
@@ -1,13 +1,28 @@
+from asyncio.coroutines import iscoroutine
 from typing import Any, NamedTuple, Dict
 
 
 class RequestBody(NamedTuple):
     url: str
-    method: str = 'GET'
-    headers: Dict[str, Any] = {'Content-Type': 'JSON'}
+    method: str = "GET"
+    headers: Dict[str, Any] = {"Content-Type": "JSON"}
     params: Any = None
     data: Any = None
 
 
-a = RequestBody('a')
+a = RequestBody("a")
 print(a._asdict())
+
+import asyncio
+
+
+class A:
+    async def test(self):
+        await asyncio.sleep(1)
+        yield 1
+
+
+from inspect import isawaitable, iscoroutinefunction
+
+a = A().test
+print(iscoroutinefunction(a))
