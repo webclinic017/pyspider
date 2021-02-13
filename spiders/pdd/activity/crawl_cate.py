a1d6f497be153bb314772e7cfd3d8279f433e820
wangxin <eric@example.com>
2021-02-13 22:31:05 +0800

    1

---
+++ spiders/pdd/activity/crawl_cate.py
@@ -0,0 +1,36 @@
+import json
+import re
+
+import requests
+
+
+def get_cat_info(url):
+    """
+    获取分类信息
+    """
+    cate_list = []
+    url = "https://mobile.yangkeduo.com/sbxeghhl.html"
+    headers = {
+        "authority": "mobile.yangkeduo.com",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "zh-CN,zh;q=0.9,ja;q=0.8,en;q=0.7",
+    }
+    ss = requests.Session()
+    ss.headers = headers
+    ss.stream = True
+    _ = ss.get(url, timeout=10, verify=False)
+    data = ss.get(url, timeout=10, verify=False).text
+    ss.close()
+    pattern = re.compile(r'{"store":.*}')
+    result = pattern.findall(data)[0]
+    result = json.loads(result)
+    tab_list = result["store"]["tabList"]
+    list_id = result["store"]["listId"]
+    for item in tab_list:
+        sub_tab = item["subTabs"]
+        cate_list.extend(sub_tab)
+    for i in cate_list:
+        i["list_id"] = list_id
+    return cate_list
--- spiders/pdd/activity/find_goods.py
+++ spiders/pdd/activity/find_goods.py
@@ -1,59 +1,25 @@
+import itertools
 import json
-import re
 import sys
 from urllib.parse import quote
-
-import requests
 
 if sys.platform == "win32":
     path = "C:\\Users\\Ety\\Desktop\\pyspider"
 else:
     path = "/data/spider/pyspider"
 sys.path.append(path)
-import itertools
 
 from common.spider import AsyncSpider
 from service.pdd_risk import PddParamsProducer
 from utils.log import get_logger
+
+from .crawl_cate import get_cat_info
 
 logger = get_logger("activity_find_goods")
 
 
 class CrawlFindGoods(AsyncSpider):
     key = "pdd_activity_find_goods"
-
-    def get_cat_id(self, only_get_listId=False):
-        """
-        获取分类信息
-        """
-        cate_list = []
-        url = "https://mobile.yangkeduo.com/sbxeghhl.html"
-        headers = {
-            "authority": "mobile.yangkeduo.com",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "zh-CN,zh;q=0.9,ja;q=0.8,en;q=0.7",
-        }
-        ss = requests.Session()
-        ss.headers = headers
-        ss.stream = True
-        _ = ss.get(url, timeout=10, verify=False)
-        data = ss.get(url, timeout=10, verify=False).text
-        ss.close()
-        pattern = re.compile(r'{"store":.*}')
-        result = pattern.findall(data)[0]
-        result = json.loads(result)
-        tab_list = result["store"]["tabList"]
-        list_id = result["store"]["listId"]
-        if only_get_listId:
-            return list_id
-        for item in tab_list:
-            sub_tab = item["subTabs"]
-            cate_list.extend(sub_tab)
-        for i in cate_list:
-            i["list_id"] = list_id
-        return cate_list
 
     @staticmethod
     def make_body(cate_info, anti_content, page):
@@ -82,7 +48,8 @@ class CrawlFindGoods(AsyncSpider):
         return headers
 
     async def start_requests(self):
-        cate_list = self.get_cat_id()
+        cate_url = "https://mobile.yangkeduo.com/sbxeghhl.html"
+        cate_list = get_cat_info(cate_url)
         url = "https://mobile.yangkeduo.com/proxy/api/api/lithium/query/goods_list?pdduid=0"
         api_uid = "CkmjnGATsRdMFQBWrnXTAg=="
         referer = "http://mobile.yangkeduo.com/sbxeghhl.html?_pdd_fs=1&_pdd_nc=ffffff&_pdd_tc=00ffffff&_pdd_sbs=1&refer_page_name=index"
@@ -107,4 +74,4 @@ class CrawlFindGoods(AsyncSpider):
 
 
 if __name__ == "__main__":
-    CrawlFindGoods.start(env="redis15")
+    CrawlFindGoods.start()
