eaaa10a40cc8cec8f089ba85b658a5a380e0d3b5
wangxin <wangxin@91cyt.com>
2021-02-20 12:29:52 +0800

    1

--- utils/tools.py
+++ utils/tools.py
@@ -10,9 +10,7 @@ _ITERABLE_SINGLE_VALUES = dict, str, bytes
 
 
 def arglist_to_dict(arglist):
-    """Convert a list of arguments like ['arg1=val1', 'arg2=val2', ...] to a
-    dict
-    """
+    """Convert a list of arguments like ['arg1=val1', 'arg2=val2', ...] to a dict"""
     return dict(x.split("=", 1) for x in arglist)
 
 
