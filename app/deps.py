54c3ccc6e6826dc4c66b1d7e8e8a3578a0cfd31e
wangxin <wangxin@91cyt.com>
2021-02-25 12:06:59 +0800

    1

--- app/deps.py
+++ app/deps.py
@@ -5,5 +5,15 @@ def get_redis_local(request: Request):
     return request.app.state.redis
 
 
+def get_redis15(request: Request):
+    return request.app.state.redis15
+
+
+def get_redis30(request: Request):
+    return request.app.state.redis30
+
+
 class DBDepend:
     redis_local = Depends(get_redis_local)
+    redis15 = Depends(get_redis15)
+    redis30 = Depends(get_redis30)
