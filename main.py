8acea9ea2ae1669a4ef4faf59d9fb8a1f78e39bd
wangxin <wangxin@91cyt.com>
2021-02-25 11:58:38 +0800

    1

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ from app.events import setup_db, shutdown_db
 
 
 def create_app(env="test"):
-    app = FastAPI()
+    if env == "test":
+        debug = True
+    else:
+        debug = False
+    app = FastAPI(debug=debug)
     app.add_event_handler("startup", setup_db(app, env=env))
     app.add_event_handler("shutdown", shutdown_db(app))
+
     app.add_exception_handler(RequestValidationError, validation_error_handler)
     app.add_exception_handler(HTTPException, http_error_handler)
+
     app.include_router(api.router)
+
     return app
 
 
