ca8e1dc1c85cdcd4737d70eed715785ae611d6e8
wangxin <wangxin@91cyt.com>
2021-02-26 15:13:55 +0800

    1

--- app/errors.py
+++ app/errors.py
@@ -1,10 +1,10 @@
 from fastapi.exceptions import HTTPException, RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
 
 
 async def validation_error_handler(request, exc: RequestValidationError):
-    return JSONResponse({"errors": [exc.errors]}, status_code=400)
+    return ORJSONResponse({"errors": [exc.errors]}, status_code=400)
 
 
 async def http_error_handler(request, exc: HTTPException):
-    return JSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
+    return ORJSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
