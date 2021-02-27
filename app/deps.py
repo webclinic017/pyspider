9336f9c6649f503fd9f91f58fa4d65cee681510e
wangxin <wangxin@91cyt.com>
2021-02-27 11:59:01 +0800

    1

--- app/deps.py
+++ app/deps.py
@@ -5,9 +5,6 @@ def get_env(request: Request):
     return request.app.state.env
 
 
-env = Depends(get_env)
-
-
 def get_client_session(request: Request):
     return request.app.state.session
 
@@ -29,12 +26,7 @@ class Depend:
     全局依赖项
     """
 
+    env = Depends(get_env)
     session = Depends(get_client_session)
-
-    if env == "test":
-        redis_local = Depends(get_redis_local)
-        redis15 = redis_local
-        redis30 = redis_local
-    else:
-        redis15 = Depends(get_redis15)
-        redis30 = Depends(get_redis30)
+    redis15 = Depends(get_redis15)
+    redis30 = Depends(get_redis30)
